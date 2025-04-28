c861110a18032dc2a053d29b8e0072c0ff773dbb
mukesh0902 <ku.mukesh8474@gmail.com>
2025-04-28 10:45:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // These paths cover all JavaScript/TypeScript files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
